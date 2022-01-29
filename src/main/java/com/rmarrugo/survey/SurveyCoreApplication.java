c7e972e66dbd243c31e21eee46dac759765cfbbd
richard <richard.marrugo@itxpertix.com>
2022-01-29 15:50:46 -0500

    first commit

---
+++ src/main/java/com/rmarrugo/survey/SurveyCoreApplication.java
@@ -0,0 +1,13 @@
+package com.rmarrugo.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyCoreApplication.class, args);
+	}
+
+}
